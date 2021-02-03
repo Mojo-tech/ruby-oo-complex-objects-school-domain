5b98edb66f04c5041b2c000222c231c6cc591848
Monique <momo_89@ymail.com>
2021-02-02 23:35:56 -0600

    commit tree c0f766fe187d5fc5fb47e1fde4a524d71fafa8af

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+    attr_accessor :grade, :school_name, :roster, :student_name
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new{ |h, k| h[k] = [] }
+    end
+
+
+    def add_student(student_name, grade)
+        @roster[grade] << student_name
+    end
+
+    def grade(level)
+        roster.detect do |x, y|
+            if x == level
+                return y
+            end
+        end
+    end
+
+    def sort 
+        roster.each do |x, y|
+          roster[x] = y.sort
+        end
+        
+    end
+
+
+
+end
